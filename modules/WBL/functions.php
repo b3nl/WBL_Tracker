e5bf73e6a6dddcfa85087685611fc210b57db1b8
WBL-BjoernLange <github-bl@wbl-konzept.de>
2012-07-25 16:57:04 +0200

    * minor fixes + Callback for getting the possible payments of a checkout

--- modules/WBL/functions.php
+++ modules/WBL/functions.php
@@ -4,6 +4,7 @@
 
     spl_autoload_register(array(
         $oAutoloader
+	        ->addCoreOverride('oxsession',     'WBL_Modules_Session') // Fix against #0004262
             ->addCoreOverride('oxutilsobject', 'WBL_Modules_UtilsObject')
             ->setAutoloaderNamespaces(array('WBL')),
         'includeClass'
