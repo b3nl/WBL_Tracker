d40a0da0667ba12b21491acf3f731becf56a5160
WBL_b3nl <b.lange@wbl-konzept.de>
2013-07-02 21:51:50 +0200

    + ajax "hack" for getting better product data

--- copy_this/modules/WBL/Tracker/Piwik/Adapter/Details.php
+++ copy_this/modules/WBL/Tracker/Piwik/Adapter/Details.php
@@ -28,4 +28,28 @@
 		 * @var bool
 		 */
 		protected $bWithDefaultCat = true;
+
+		/**
+		 * Changes the URL addditionally.
+		 * @author blange <code@wbl-konzept.de>
+		 * @return WBL_Tracker_Piwik_Adapter_Details
+		 */
+		public function init() {
+			$oParent  = parent::init();
+			$oProduct = $this->getView()->getProduct();
+
+			$this->addTrackerCall(array('"setCustomUrl"', 'window.location'), false);
+
+			if ($oProduct) {
+				$this->addTrackerCall(
+					array(
+						'setDocumentTitle',
+						trim($oProduct->oxarticles__oxtitle->value . ' ' . $oProduct->oxarticles__oxvarselect->value)
+					)
+				);
+			} // if
+			unset($oProduct);
+
+			return $oParent;
+		} // function
 	} // class
