7fc8cce016743c384bb039647d5ee18e486312dd
WBL_b3nl <b.lange@wbl-konzept.de>
2014-03-26 11:20:02 +0100

    + callback

--- copy_this/modules/WBL/Tracker/Piwik/trackerdata.php
+++ copy_this/modules/WBL/Tracker/Piwik/trackerdata.php
@@ -1,34 +1,42 @@
 <?php
 	/**
 	 * ./modules/WBL/Tracker/Piwik/trackerdata.php
-	 * @author blange <code@wbl-konzept.de>
-	 * @category modules
-	 * @package WBL_Tracker
+	 * @author     blange <code@wbl-konzept.de>
+	 * @category   modules
+	 * @package    WBL_Tracker
 	 * @subpackage Piwik
-	 * @version SVN: $Id$
+	 * @version    SVN: $Id$
 	 */
 
 	/**
 	 * Meta-Data for the piwik tracker.
-	 * @author blange <code@wbl-konzept.de>
-	 * @category modules
-	 * @package WBL_Tracker
+	 * @author     blange <code@wbl-konzept.de>
+	 * @category   modules
+	 * @package    WBL_Tracker
 	 * @subpackage Piwik
-	 * @version SVN: $Id$
+	 * @version    SVN: $Id$
 	 */
 	return array(
 		'settings' => array(
-			array('name' => 'aWBLTrackerPiwikGoals',       'type' => 'aarr', 'value' => array()),
-			array('name' => 'sWBLTrackerPiwikTrackerURL',  'type' => 'str', 'value' => 'example.com'),
-			array('name' => 'sWBLTrackerPiwikSiteId',      'type' => 'str', 'value' => 0),
-			array('name' => 'aWBLTrackerPiwikPageVars',    'type' => 'aarr', 'value' => array(
-				1 => 'searchhits|WBL_Tracker_Conditions_Value_Search_Hits||search',
-				'searchquery|WBL_Tracker_Conditions_Value_Search_Query||search',
-				4 => 'orderpayment|WBL_Tracker_Conditions_Value_OrderPayment||thankyou',
-				'possiblepayments|WBL_Tracker_Conditions_Value_Search_PossiblePayments||payment',
-			)),
-			array('name' => 'aWBLTrackerPiwikVisitVars',   'type' => 'aarr', 'value' => array(
-				3 => 'userstatus|WBL_Tracker_Conditions_Value_UserStatus'
-			))
+			array('name' => 'aWBLTrackerPiwikGoals', 'type' => 'aarr', 'value' => array()),
+			array('name' => 'sWBLTrackerPiwikTrackerURL', 'type' => 'str', 'value' => 'example.com'),
+			array('name' => 'sWBLTrackerPiwikSiteId', 'type' => 'str', 'value' => 0),
+			array(
+				'name'  => 'aWBLTrackerPiwikPageVars',
+				'type'  => 'aarr',
+				'value' => array(
+					1 => 'searchhits|WBL_Tracker_Conditions_Value_Search_Hits||search',
+					'searchquery|WBL_Tracker_Conditions_Value_Search_Query||search',
+					4 => 'orderpayment|WBL_Tracker_Conditions_Value_OrderPayment||thankyou',
+					'possiblepayments|WBL_Tracker_Conditions_Value_PossiblePayments||payment',
+				)
+			),
+			array(
+				'name'  => 'aWBLTrackerPiwikVisitVars',
+				'type'  => 'aarr',
+				'value' => array(
+					3 => 'userstatus|WBL_Tracker_Conditions_Value_UserStatus'
+				)
+			)
 		)
 	);
