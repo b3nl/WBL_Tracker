b9535788518f02f5da16253f230df5aa9eb14702
WBL_b3nl <b.lange@wbl-konzept.de>
2014-03-26 16:02:22 +0100

    + new widget control * style * new number handling for faulty piwik

--- copy_this/modules/WBL/Tracker/Output.php
+++ copy_this/modules/WBL/Tracker/Output.php
@@ -1,105 +1,111 @@
 <?php
-	/**
-	 * ./modules/WBL/Tracker/Output.php
-	 * @author Bjoern Simon Lange <code@wbl-konzept.de>
-	 * @category modules
-	 * @package WBL_Tracker
-	 * @subpackage oxOutput
-	 * @version SVN: $Id$
-	 */
+    /**
+     * ./modules/WBL/Tracker/Output.php
+     * @author     Bjoern Simon Lange <code@wbl-konzept.de>
+     * @category   modules
+     * @package    WBL_Tracker
+     * @subpackage oxOutput
+     * @version    SVN: $Id$
+     */
 
-	/**
-	 * Extension for oxOutput.
-	 * @author Bjoern Simon Lange <code@wbl-konzept.de>
-	 * @category modules
-	 * @package WBL_Tracker
-	 * @subpackage oxOutput
-	 * @todo Testing of oxshopcontrol and oxemail
-	 * @version SVN: $Id$
-	 */
-	class WBL_Tracker_Output extends WBL_Tracker_Output_parent {
-		/**
-		 * The view data of the oxid process.
-		 * @var array
-		 */
-		private $_aViewDataForTracker = array();
+    /**
+     * Extension for oxOutput.
+     * @author     Bjoern Simon Lange <code@wbl-konzept.de>
+     * @category   modules
+     * @package    WBL_Tracker
+     * @subpackage oxOutput
+     * @todo       Testing of oxshopcontrol and oxemail
+     * @version    SVN: $Id$
+     */
+    class WBL_Tracker_Output extends WBL_Tracker_Output_parent
+    {
+        /**
+         * The view data of the oxid process.
+         * @var array
+         */
+        private $_aViewDataForTracker = array();
 
-		/**
-		 * Returns the used view data.
-		 * @author blange <code@wbl-konzept.de>
-		 * @return array
-		 */
-		protected function getViewData() {
-			return $this->_aViewDataForTracker;
-		} // function
+        /**
+         * Returns the used view data.
+         * @author blange <code@wbl-konzept.de>
+         * @return array
+         */
+        protected function getViewData()
+        {
+            return $this->_aViewDataForTracker;
+        } // function
 
-		/**
-		 * (non-PHPdoc)
-		 * @see http/core/oxOutput::process()
-		 */
-		public function process($sValue, $sClassName) {
-			$sReturn = parent::process($sValue, $sClassName);
+        /**
+         * (non-PHPdoc)
+         * @see http/core/oxOutput::process()
+         */
+        public function process($sValue, $sClassName)
+        {
+            $sReturn = parent::process($sValue, $sClassName);
 
-			if (!((strpos($sClassName, 'oxw') !== 0) && (!$this->getConfig()->isAdmin()) &&
-				($aAdapters = WBL_Tracker_Adapter_Loader::getInstance()->getAdaptersForClass($sClassName))))
-			{
-				return $sReturn;
-			} // if
+            if ((strpos($sClassName, 'oxw') !== 0) && (strpos(get_parent_class($sClassName), 'oxw') !== 0) &&
+                (!$this->getConfig()->isAdmin()) &&
+                ($aAdapters = WBL_Tracker_Adapter_Loader::getInstance()->getAdaptersForClass($sClassName)))
+            {
+                $oStr          = getStr();
+                $sTrackerCodes = $this->processWBLAdapters($aAdapters, $sClassName);
 
-			$oStr          = getStr();
-			$sTrackerCodes = $this->processWBLAdapters($aAdapters, $sClassName);
+                // TODO Verzweigung ob top or bottom.
+                if ($oStr->strpos($sValue, $sSearch = '</body>') !== false) {
+                    $sReturn = $oStr->preg_replace(
+                        '#' . $sSearch . '#',
+                        $sTrackerCodes . $sSearch,
+                        $sValue
+                    );
+                } elseif (strtolower($sClassName) === 'oxemail') {
+                    $sReturn = ($sReturn . $sTrackerCodes);
+                } // elseif
+            } // if
 
-			// TODO Verzweigung ob top or bottom.
-			if ($oStr->strpos($sValue, $sSearch = '</body>') !== false) {
-				return $oStr->preg_replace(
-					'#' . $sSearch . '#',
-					$sTrackerCodes . $sSearch,
-					$sValue
-				);
-			} // if
+            return $sReturn;
+        } // function
 
-			return (strtolower($sClassName) === 'oxemail') ? $sReturn : ($sReturn . $sTrackerCodes);
-		} // function
+        /**
+         * (non-PHPdoc)
+         * @see http/core/oxOutput::processViewArray()
+         */
+        public function processViewArray($aData, $sClass)
+        {
+            $aData = parent::processViewArray($aData, $sClass);
+            return $this->_aViewDataForTracker = $aData;
+        } // function
 
-		/**
-		 * (non-PHPdoc)
-		 * @see http/core/oxOutput::processViewArray()
-		 */
-		public function processViewArray($aData, $sClass) {
-			$aData = parent::processViewArray($aData, $sClass);
-			return $this->_aViewDataForTracker = $aData;
-		} // function
+        /**
+         * Concatinates the HTML of the tracker.
+         * @author blange <code@wbl-konzept.de>
+         * @param WBL_Tracker_Adapter_Interface[] $aAdapters
+         * @param string                          $sClassName
+         * @return string
+         */
+        protected function processWBLAdapters(array $aAdapters, $sClassName)
+        {
+            $aArray  = $this->getViewData();
+            $oSmarty = oxUtilsView::getInstance()->getSmarty();
+            $oView   = null;
+            $sHTML   = '';
 
-		/**
-		 * Concatinates the HTML of the tracker.
-		 * @author blange <code@wbl-konzept.de>
-		 * @param WBL_Tracker_Adapter_Interface[] $aAdapters
-		 * @param string $sClassName
-		 * @return string
-		 */
-		protected function processWBLAdapters(array $aAdapters, $sClassName) {
-			$aArray  = $this->getViewData();
-			$oSmarty = oxUtilsView::getInstance()->getSmarty();
-			$oView   = null;
-			$sHTML   = '';
+            if (strtolower($sClassName) !== 'oxemail') {
+                $oView = $this->getConfig()->getActiveView();
+            } // if
 
-			if (strtolower($sClassName) !== 'oxemail') {
-				$oView = $this->getConfig()->getActiveView();
-			} // if
+            /* @var $oAdapter WBL_Tracker_Adapter_Interface */
+            foreach ($aAdapters as $oAdapter) {
+                $oAdapter->setSmarty($oSmarty)->setViewData($aArray);
 
-			/* @var $oAdapter WBL_Tracker_Adapter_Interface */
-			foreach ($aAdapters as $oAdapter) {
-				$oAdapter->setSmarty($oSmarty)->setViewData($aArray);
+                if ($oView) {
+                    $oAdapter->setView($oView);
+                } // if
 
-				if ($oView) {
-					$oAdapter->setView($oView);
-				} // if
+                $oAdapter->init();
+                $sHTML .= $oAdapter->getHTML();
+            } // foreach
+            unset($aAdapters, $aArray, $oAdapter, $oSmarty, $oView);
 
-				$oAdapter->init();
-				$sHTML .= $oAdapter->getHTML();
-			} // foreach
-			unset($aAdapters, $aArray, $oAdapter, $oSmarty, $oView);
-
-			return $sHTML;
-		} // function
-	} // class
+            return $sHTML;
+        } // function
+    } // class
--- copy_this/modules/WBL/Tracker/metadata.php
+++ copy_this/modules/WBL/Tracker/metadata.php
@@ -1,34 +1,34 @@
 <?php
 	/**
 	 * Module-Metadata for the WBL Tracker.
-	 * @author     blange <code@wbl-konzept.de>
-	 * @category   modules
-	 * @package    WBL_Modules
+	 * @author blange <code@wbl-konzept.de>
+	 * @category modules
+	 * @package WBL_Modules
 	 * @subpackage oxAutoload
-	 * @version    SVN: $Id$
+	 * @version SVN: $Id$
 	 */
 
 	$sMetadataVersion = '1.0';
 
 	$aModule = array(
-		'author'      => 'WBL Konzept',
-		'description' => array(
+		'author'       => 'WBL Konzept',
+		'description'  => array(
 			'de' => 'Framework um verschiedenste Tracker mit wenig Anpassungen zu implementieren',
 			'en' => 'Special Framework for integrating various tracker'
 		),
-		'email'       => 'code@wbl-konzept.de',
-		'extend'      => array(
+		'email'        => 'code@wbl-konzept.de',
+		'extend'       => array(
 			'oxbasket'     => 'WBL_Tracker_Basket',
 			'oxbasketitem' => 'WBL_Tracker_Basket_Item',
 			'oxoutput'     => 'WBL_Tracker_Output',
 		),
-		'files'       => array(
+		'files'        => array(
 			'WBL_Tracker_Basket'      => 'WBL/Tracker/Basket.php',
 			'WBL_Tracker_Basket_Item' => 'WBL/Tracker/Basket/Item.php',
 			'WBL_Tracker_Output'      => 'WBL/Tracker/Output.php'
 		),
-		'id'          => 'WBL_Tracker',
-		'settings'    => array_merge(
+		'id'           => 'WBL_Tracker',
+		'settings'     => array_merge(
 			array(
 				array(
 					'group' => 'WBL_Tracker_Main',
@@ -41,8 +41,8 @@
 				? WBL_Tracker_Adapter_Loader::getInstance()->getExtendArrayForMetaData()
 				: array()
 		),
-		'title'       => 'WBL Webtracking Framework',
-		'thumbnail'   => 'wbl_logo.jpg',
-		'url'         => 'http://wbl-konzept.de',
-		'version'     => '1.0.0'
+		'title'        => 'WBL Webtracking Framework',
+		'thumbnail'    => 'wbl_logo.jpg',
+		'url'          => 'http://wbl-konzept.de',
+		'version'      => '1.0.0'
 	);
